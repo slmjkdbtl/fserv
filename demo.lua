f04f4584e740d8afee953d7062a57baf970b1281
tga <tga@space55.xyz>
2020-11-19 02:36:10 -0500

    README

--- demo.lua
+++ demo.lua
@@ -1,19 +1,9 @@
 -- wengwengweng
 
-local t = httph.tag
-
-local page = t("html", {}, {
-	t("head", {}, {}),
-	t("body", {}, {
-		t("p", {}, "tga"),
-		t("a", { href = "https://enemyspy.xyz", }, "here"),
-	}),
-})
-
 http.serve(8000, function(req)
 	return {
 		status = 200,
-		body = page,
+		body = "hi",
 	}
 end)
 
